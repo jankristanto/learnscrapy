7a1f16a39ca1681f2bc728c6e786ab254925b4f0
Jan Kristanto <jan@ontelstudio.com>
2014-04-15 15:36:39 +0700

    initial

---
+++ learnscrapy/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for learnscrapy project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'learnscrapy'
+
+SPIDER_MODULES = ['learnscrapy.spiders']
+NEWSPIDER_MODULE = 'learnscrapy.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'learnscrapy (+http://www.yourdomain.com)'
